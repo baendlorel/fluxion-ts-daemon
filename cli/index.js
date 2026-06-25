0cff57f2c56835d1150b2a986de80ff4227a6777
aldia_mechdream <futami16237@gmail.com>
2026-06-25 15:51:37 +0800

    现在会detached运行

--- cli/index.js
+++ cli/index.js
@@ -1,9 +1,14 @@
+import { spawn } from 'node:child_process';
 import os from 'node:os';
 
 const platform = os.platform();
 switch (platform) {
   case 'linux':
     console.log('Linux');
+    spawn('fluxion-daemon', ['--command="tsx main.ts"'], {
+      detached: true,
+      stdio: 'ignore',
+    });
     break;
   default:
     console.log('Not Supported Yet: ' + platform);
